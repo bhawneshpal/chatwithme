42289552e198b0f3298e37a4a4352699ffcc539e
06pratul-pal <06pratul@gmail.com>
2026-07-06 00:27:10 +0530

    1 - chatwithme

---
+++ oops_proj1.py
@@ -0,0 +1,73 @@
+class chatwithme:
+    def __init__(self):
+        self.username = ""
+        self.password = ""
+        self.login = False
+        self.menu()
+
+
+    def menu(self):
+        user_input = input("""Welcome to chatwithme!!
+                           How would you like to proceed
+                           1. Press 1 to signup
+                           2. Press 2 to signin 
+                           3. Press 3 to write a post
+                           4. Press 4 to message a friend
+                           5. Press any other key to exit
+                           
+                           """
+                             )
+        
+        if user_input == "1":
+            self.signup()
+        elif  user_input == "2":
+            self.signin()
+        elif  user_input == "3":
+            self.my_post
+        elif  user_input == "4":
+            self.send_msg
+        else :
+            exit()
+
+    def signup(self):
+        email = input("enter your email here :-")
+        pwd = input("setup your password here :-")
+        self.username == email
+        self.password == pwd
+        print("You have signed up successfully")
+        print("\n")
+        self.menu()
+
+    def signin(self):
+        if self.username == "" and self.password == "":
+            print('Please signup first by pressing 1 in the main menu')
+        else:
+            uname = input('enter your email/username here')
+            pwd = input('Enter your password here')
+            if self.username == uname and self.password == pwd:
+                print('You have signed in successfully')
+                self.login = True
+            else:
+                print('Please input correct credentials..')
+
+        print("\n")
+        self.menu()
+    def my_post(self):
+        if self.login == True:
+            txt = input("what's on your mind")
+            print (f"Following content has been posted {txt}")
+        else:
+            print("You need to signup first")
+        print("\n")
+        self.menu()
+
+    
+
+    def send_msg(self):
+        if self.login == True:
+            txt = input('Enter your message here')
+            frnd = input("whom to send the msg?")
+            print(f'Your message has been sent to {frnd}')
+        else :
+            print('you need to sign in first to send message')
+obj = chatwithme()
